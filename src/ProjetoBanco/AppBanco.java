6e01396858aeeb5bf4d7beb8187dbdbd1b3f19b9
Jair Gudaites Jr <jair_gudaites_jr@hotmail.com>
2018-11-13 15:04:33 -0200

    Inclusão Main e ajustes no GerenciadoContas

--- src/ProjetoBanco/AppBanco.java
+++ src/ProjetoBanco/AppBanco.java
@@ -25,6 +25,9 @@ public class AppBanco {
         double saldo;
         int limite;
         String nomeGerente;
+        int numeroContaOrigem;
+        int numeroContaDestino;
+        double valor;
 
         int opcao;
         int opcao2;
@@ -32,14 +35,15 @@ public class AppBanco {
         Scanner in = new Scanner(System.in);
         System.out.println("\t\tAPLICATIVO BANCARIO\n");
         do {
-            System.out.println("Menu:\n"
+            System.out.println("\nMenu:\n"
             +"1 - Adicionar Conta\n"
             +"2 - Remover Conta\n"
             +"3 - Buscar Contas Especias\n"
             +"4 - Busca de Cliente por Limite de Crédito\n"
             +"5 - Buscar Conta por Número\n"
             +"6 - Transferência\n"
-            +"7 - Depósito\n"
+            +"7 - Sacar\n"
+            +"8 - Depositar\n"
             +"9 - Listar Contas Cadastradas\n"
             +"10 - Sair\n");
 
@@ -59,17 +63,16 @@ public class AppBanco {
                     switch (opcao2) {
                         case 1:
                             System.out.println("Insira os seguintes dados:");
-                            System.out.println("CPF: ");
+                            System.out.printf("CPF: ");
                             cpf = in.nextLine();
                             System.out.printf("Nome: ");
                             nomeCliente = in.nextLine();
-                            System.out.println("Conta: ");
+                            System.out.printf("Conta: ");
                             numeroConta = in.nextInt();
                             in.nextLine();
-                            System.out.println("Saldo: ");
+                            System.out.printf("Saldo: ");
                             saldo = in.nextDouble();
                             in.nextLine();
-                            System.out.println("Possui limite(s/n)?");
                             int random = r.nextInt(2);
                             if (random == 1) {
                                 limite = 1000;
@@ -80,14 +83,14 @@ public class AppBanco {
                             break;
                         case 2:
                             System.out.println("Insira os seguintes dados:");
-                            System.out.println("CPF: ");
+                            System.out.printf("CPF: ");
                             cpf = in.nextLine();
                             System.out.printf("Nome: ");
                             nomeCliente = in.nextLine();
-                            System.out.println("Conta: ");
+                            System.out.print("Conta: ");
                             numeroConta = in.nextInt();
                             in.nextLine();
-                            System.out.println("Saldo: ");
+                            System.out.print("Saldo: ");
                             saldo = in.nextDouble();
                             in.nextLine();
                             gerenciadorContas.adicionarConta(new ContaPoupanca(cpf, nomeCliente, numeroConta, saldo));
@@ -95,26 +98,102 @@ public class AppBanco {
 
                         case 3:
                             System.out.println("Insira os seguintes dados:");
-                            System.out.println("CPF: ");
+                            System.out.printf("CPF: ");
                             cpf = in.nextLine();
                             System.out.printf("Nome: ");
                             nomeCliente = in.nextLine();
-                            System.out.println("Conta: ");
+                            System.out.printf("Conta: ");
                             numeroConta = in.nextInt();
                             in.nextLine();
-                            System.out.println("Saldo: ");
+                            System.out.printf("Saldo: ");
                             saldo = in.nextDouble();
                             in.nextLine();
                             limite = 1000;
-                            System.out.println("Nome Gerente");
+                            System.out.printf("Nome Gerente: ");
                             nomeGerente = in.nextLine();
                             gerenciadorContas.adicionarConta(new ContaEspecial(cpf,nomeCliente,numeroConta,saldo,limite,nomeGerente));
                             break;
+                        default:
+                            System.out.println("Opção Inválida");
+                            break;
                     }
+                    break;
+                case 2:
+                    System.out.println("Insira o número da conta a ser excluida: ");
+                    numeroConta = in.nextInt();
+                        if (gerenciadorContas.removerConta(numeroConta)){
+                            System.out.println("Conta Removida\n");
+                        } else {
+                            System.out.println("Conta não encontrada\n");
+                        }
+                    break;
+                case 3:
+                    System.out.println("Contas Especiais Encontradas");
+                    if (gerenciadorContas.buscarContasEpeciais()==""){
+                        System.out.println("Nenhuma Conta Especial Encontrada\n");
+                    } else {
+                        System.out.println(gerenciadorContas.buscarContasEpeciais()+"\n");
+                        }
+                    break;
+                case 4:
+                    System.out.println("Clientes Encontrados");
+                    if (gerenciadorContas.buscarClientesUsandoLimite()==""){
+                        System.out.println("Nenhum Cliente Encontrado\n");
+                    } else{
+                        System.out.println(gerenciadorContas.buscarClientesUsandoLimite()+"\n");
+                    }
+                    break;
+                case 5:
+                    System.out.printf("Insira o número da conta: ");
+                    numeroConta = in.nextInt();
+                    System.out.println(gerenciadorContas.buscarConta(numeroConta)+"\n");
+                    break;
+                case 6:
+                    System.out.printf("Insira a conta de Origin: ");
+                    numeroContaOrigem = in.nextInt();
+                    System.out.printf("Insira a conta de Destino: ");
+                    numeroContaDestino = in.nextInt();
+                    System.out.printf("Insira o Valor: ");
+                    valor = in.nextDouble();
+                    if (gerenciadorContas.transferirValor(numeroContaOrigem,numeroContaDestino,valor)){
+                        System.out.printf("Transferência Realizada com Sucesso\n");
+                    } else {
+                        System.out.printf("Transferência não Realizada\n");
+                    }
+                    break;
+                case 7:
+                    System.out.printf("Insira Número da Conta:");
+                    numeroConta = in.nextInt();
+                    System.out.printf("Insira o Valor: ");
+                    valor = in.nextDouble();
+                    if (gerenciadorContas.sacar(numeroConta,valor)){
+                        System.out.println("Saque Realizado com Sucesso\n");
+                    } else {
+                        System.out.println("Saque não Realizado\n");
+                    }
+                    break;
+                case 8:
+                    System.out.printf("Insira Número da Conta:");
+                    numeroConta = in.nextInt();
+                    System.out.printf("Insira o Valor: ");
+                    valor = in.nextDouble();
+                    if (gerenciadorContas.depositar(numeroConta,valor)){
+                        System.out.println("Depósito Realizado com Sucesso\n");
+                    } else {
+                        System.out.println("Depósito não Realizado\n");
+                    }
+                    break;
+                case 9:
+                    System.out.println("Contas Cadastradas: ");
+                    System.out.println(gerenciadorContas.listarContas());
+                    break;
+                case 10:
+                    System.out.println("Programa Finalizado");
+                    break;
+                default:
+                    System.out.println("Opção Inválida");
+                    break;
             }
-
-        }while (opcao==10);
-
+        }while (opcao!=10);
     }
-    
 }
