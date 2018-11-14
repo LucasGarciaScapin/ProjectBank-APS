b1995142a24015f06d2532107435aead1242306f
Lucas Garcia Scapin <lucas.scapin@keyrus.com.br>
2018-11-14 09:58:54 -0200

    Arrumando testes

--- src/ProjetoBanco/ProjetoBancoTeste/ContaCorrenteTeste.java
+++ src/ProjetoBanco/ProjetoBancoTeste/ContaCorrenteTeste.java
@@ -8,15 +8,16 @@ import static org.junit.Assert.assertTrue;
 
 public class ContaCorrenteTeste {
 
+    ContaCorrente c1 = new ContaCorrente("039.444.251-07","Lucas Garcia",25874,5000,2000);
+    ContaCorrente c2 = new ContaCorrente("039.444.251-07","Lucas Garcia",25874,5000,2000);
+
     @Test
     public void testeUsandoLimite(){
 
         // Teste Unitario - UsandoLimite()
-        ContaCorrente c1 = new ContaCorrente("039.444.251-07","Lucas Garcia",25874,5000,2000);
         c1.sacar(6000);
         assertTrue(c1.usandoLimite());
 
-        ContaCorrente c2 = new ContaCorrente("039.444.251-07","Lucas Garcia",25874,5000,2000);
         c2.sacar(4000);
         assertFalse(c2.usandoLimite());
 
@@ -24,16 +25,8 @@ public class ContaCorrenteTeste {
 
     @Test
     public void testeSacar(){
-
-        ContaCorrente c1 = new ContaCorrente("039.444.251-07","Lucas Garcia",25874,5000,2000);
         assertTrue(c1.sacar(3500));
-
-        ContaCorrente c2 = new ContaCorrente("039.444.251-07","Lucas Garcia",25874,5000,2000);
         assertFalse(c2.sacar(10000));
 
     }
-
-
-
-
 }
--- src/ProjetoBanco/ProjetoBancoTeste/GerenciadorContasTeste.java
+++ src/ProjetoBanco/ProjetoBancoTeste/GerenciadorContasTeste.java
@@ -1,33 +1,80 @@
 package ProjetoBanco.ProjetoBancoTeste;
 
 import ProjetoBanco.ContaCorrente;
+import ProjetoBanco.ContaEspecial;
+import ProjetoBanco.ContaPoupanca;
 import ProjetoBanco.GerenciadorContas;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+
+/**
+ * @author Matheus Oliveira
+ * @author Lucas Garcia
+ */
+
 public class GerenciadorContasTeste {
 
     GerenciadorContas gc = new GerenciadorContas();
 
+    ContaCorrente cc = new ContaCorrente("205.518.520-85","Lucas Garcia",65421,10000.00,5500);
+    ContaEspecial ce = new ContaEspecial("205.520.518-85","Lucas Garcia",12356,10000.00,"Marcos Paulo");
+    ContaPoupanca cp = new ContaPoupanca("518.518.520-85","Lucas Garcia",12654,10000.00);
+
+
     @Test
     public void testeAdicionarContas(){
-        ContaCorrente cc = new ContaCorrente("205.518.520-85","Lucas Garcia",29874,25800.00,5500);
         gc.adicionarConta(cc);
         assertFalse("Retorno de lista com uma conta",gc.getList().isEmpty());
     }
 
     @Test
     public void testeRemoverContas(){
-        ContaCorrente cc = new ContaCorrente("205.518.520-85","Lucas Garcia",25874,20000.00,5000);
         gc.adicionarConta(cc);
-        gc.removerConta(25874);
-        assertTrue("Retorno de lista vazia",gc.getList().isEmpty());
-
+        gc.removerConta(65421);
+        assertTrue("Retorno de lista vazia", gc.getList().isEmpty());
     }
 
     @Test
     public void testeBuscarContasEpeciais(){
+        gc.adicionarConta(cc);
+        gc.adicionarConta(ce);
+        gc.adicionarConta(cp);
+        assertEquals("Retorno de iguais",ce + "\n",gc.buscarContasEpeciais());
+    }
 
+    @Test
+    public void testeBuscarClientesUsandoLimite(){
+        gc.adicionarConta(cc);
+        cc.sacar(11000);
+        assertEquals("Retorno de iguais", cc + "\n", gc.buscarClientesUsandoLimite());
+    }
+
+    @Test
+    public void testeBuscarConta(){
+        gc.adicionarConta(cc);
+        assertEquals(cc,gc.buscarConta(65421));
+    }
+
+    @Test
+    public void testeTransferirValor(){
+        gc.adicionarConta(cc);
+        gc.adicionarConta(cp);
+        assertTrue(gc.transferirValor(65421,12654,500.00));
+    }
+
+    @Test
+    public void testeSacar(){
+        gc.adicionarConta(cc);
+        gc.sacar(65421,2000);
+        assertEquals(8000,cc.getSaldo(),1);
+    }
+
+    @Test
+    public void testeDepositar(){
+        gc.adicionarConta(cc);
+        gc.depositar(65421,2000);
+        assertEquals(12000,cc.getSaldo(),1);
     }
 }
